28f52e8b58c3b26a7f51ed2500e86dbaa2cce15f
Ilia Donchenko <iliad@me.com>
2018-08-16 14:12:31 +0300

    doc file for swagger

---
+++ pkg/router/handlers/doc.go
@@ -0,0 +1,13 @@
+// Package router Kube-Importer
+//
+// Resource-Service is service for importing kubernetes resources to other services.
+//
+//  Version: 0.0.1
+//	Consumes:
+//	- application/json
+//
+// 	Produces:
+//	- application/json
+//
+// swagger:meta
+package handlers
